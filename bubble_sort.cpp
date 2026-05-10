fc0c435b7d48341b3ae593be91c3cf17407cda44
hitchcliff <hitchcliff1@gmail.com>
2026-05-10 12:47:54 +0800

    cout

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -8,11 +8,15 @@ int main() {
     int array[] = {64, 34, 25, 12, 22, 11, 90};
     int size = sizeof(array) / sizeof(array[0]);
 
+
     if(size <= 1) {
         std::cout << "Array is already sorted." << std::endl;
     } else {
         int* sortedArray = bubbleSort(array, size);
-        std::cout << "Sorted array: " << sortedArray[0] << " " << sortedArray[1] << " " << sortedArray[2] << " " << sortedArray[3] << " " << sortedArray[4] << " " << sortedArray[5] << " " << sortedArray[6] << std::endl;
+        std::cout << "Sorted array: ";
+        for(int arr : array) {
+            std::cout << arr << " ";
+        }
     }
 
     return 0;
@@ -22,6 +26,7 @@ int* bubbleSort(int arr[], int size) {
     int temp = arr[0];
 
     for(int i=0; i<size - 1; i++) {
+        // std::cout << arr[i] << std::endl;
         for(int j=0; j< size - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
               temp = arr[j]; 
